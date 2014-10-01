34d84755731044a802dc2e7d4a966c6b733eecbf
Manriel <manriel@mail.ru>
2014-10-01 14:56:08 +0400

    Add ACP Addon

---
+++ WTF/Account/DEFEATEDANGEL/SavedVariables/BasicChatMods.lua
@@ -0,0 +1,35 @@
+
+bcmDB = {
+	["stampformat"] = "[%I:%M:%S]",
+	["lines"] = {
+		["ChatFrame1"] = 1000,
+	},
+	["stampcolor"] = "|cff777777",
+	["replacements"] = {
+		"[GEN]", -- [1]
+		"[T]", -- [2]
+		"[WD]", -- [3]
+		"[LD]", -- [4]
+		"[LFG]", -- [5]
+		"[GR]", -- [6]
+		"[I]", -- [7]
+		"[IL]", -- [8]
+		"[G]", -- [9]
+		"[P]", -- [10]
+		"[PL]", -- [11]
+		"[PL]", -- [12]
+		"[O]", -- [13]
+		"[R]", -- [14]
+		"[RL]", -- [15]
+		"[RW]", -- [16]
+		"[%1]", -- [17]
+	},
+	["playerLBrack"] = "[",
+	["playerRBrack"] = "]",
+	["sticky"] = {
+		["EMOTE"] = 1,
+		["YELL"] = 1,
+		["RAID_WARNING"] = 1,
+	},
+	["playerSeparator"] = ":",
+}
